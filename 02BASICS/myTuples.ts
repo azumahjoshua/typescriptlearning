328b997d89230c0f943093259a679904468fdf24
azumahjoshua <joshuaazumah1086@gmail.com>
2022-11-15 13:15:25 +0100

    tuples

---
+++ 02BASICS/myTuples.ts
@@ -0,0 +1,19 @@
+let tUser: [string, number, boolean]
+
+tUser = ["ab", 123, true]
+
+let rgb: [number, number, number] = [255, 255, 124]
+
+type User = [number, string]
+
+const newUsere: User = [123, "someoone@gmail.com"]
+
+// Tuple type variable 
+let employee: [number, string] = [1, "Steve"];
+
+var person: [number, string, boolean] = [1, "Steve", true];
+
+var user: [number, string, boolean, number, string];// declare tuple variable
+user = [1, "Steve", true, 20, "Admin"];// initialize tuple variable
+
+newUsere.push()
