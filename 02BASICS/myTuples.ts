eb93f05adf5adf4bf1fbc38825dbcd9d96de437c
azumahjoshua <joshuaazumah1086@gmail.com>
2022-11-15 13:15:38 +0100

    tuples

--- 02BASICS/myTuples.ts
+++ 02BASICS/myTuples.ts
@@ -10,8 +10,13 @@ const newUsere: User = [123, "someoone@gmail.com"]
 
 // Tuple type variable 
 let employee: [number, string] = [1, "Steve"];
-
+// Add Elements into Tuple
+employee.push(2, "Bill"); 
+console.log(employee); //Output: [1, 'Steve', 2, 'Bill']
 var person: [number, string, boolean] = [1, "Steve", true];
+// retrieving value by index and performing an operation 
+employee[1] = employee[1].concat(" Jobs"); 
+console.log(employee); //Output: [1, 'Steve Jobs']
 
 var user: [number, string, boolean, number, string];// declare tuple variable
 user = [1, "Steve", true, 20, "Admin"];// initialize tuple variable
